02a61cb6e59297bb05916b78f49db718fab4cfe1
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2023-08-31 02:06:13 +0500

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const fetchProducts = async () => {
     setLoading(true);
-    const response = await fetch("http://localhost:9999/products/all");
+    const response = await fetch("https://fancy-trousers-ox.cyclic.app/products/all");
     const data = await response.json();
     setProducts(data.products);
     setLoading(false);
